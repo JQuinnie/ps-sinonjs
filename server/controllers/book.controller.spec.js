a5b3d44bb09be2f6a0cbf0b1778322956a337d6a
jquinnie <jenn8866@gmail.com>
2019-05-27 21:01:21 -0600

    Fix last test and restore

--- server/controllers/book.controller.spec.js
+++ server/controllers/book.controller.spec.js
@@ -5,11 +5,17 @@ const sinon = require('sinon');
 const controller = require('./book.controller');
 const model = require('../models').book;
 
+afterEach(() => {
+  sinon.restore();
+});
+
 describe.only('Books controller', () => {
   describe('When getting a list of books', () => {
     it('Should return 4 books', () => {
       const req = httpMocks.createRequest();
       const res = httpMocks.createResponse();
+
+      sinon.stub(model, 'all').resolves([{}, {}, {}, {}]);
 
       return controller.list(req, res).then(() => {
         return expect(res._getData().length).to.eql(4);
@@ -50,8 +56,30 @@ describe.only('Books controller', () => {
 
         const res = httpMocks.createResponse();
 
+        const find = sinon.stub(model, 'findById');
+        find.withArgs(7).resolves(null);
+
         return controller.getById(req, res).then(() => {
           return expect(res.statusCode).to.eql(404);
+        });
+      });
+    });
+
+    describe('and the book does exist', () => {
+      it('Should return a 200', () => {
+        const req = httpMocks.createRequest({
+          params: {
+            id: 7
+          }
+        });
+
+        const res = httpMocks.createResponse();
+
+        const find = sinon.stub(model, 'findById');
+        find.resolves({});
+
+        return controller.getById(req, res).then(() => {
+          return expect(res.statusCode).to.eql(200);
         });
       });
     });
